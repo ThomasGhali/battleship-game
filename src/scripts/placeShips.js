b44ec4c74cde7b8b279ee1a0ea7579e69aeda58e
Thomas <thomasmetias29@gmail.com>
2025-08-14 13:39:50 +0300

    feat: add valid and invalid preview logic & styles

--- src/scripts/placeShips.js
+++ src/scripts/placeShips.js
@@ -42,9 +42,9 @@ export function initPlaceScreen() {
     }
   })
 
-  const shipImages = document.querySelectorAll('.ship__img');
+  /* --- Ship selecting --- */
+
   let selectedShip = null;
-  let validity = false;
 
   shipsWindow.addEventListener('click', (event) => {
     if (!event.target.closest(".ship__img")) return;
@@ -70,26 +70,49 @@ export function initPlaceScreen() {
     selectedShip = ship;
   })
 
-  const board = document.querySelector('.placing-board');
-  board.addEventListener('click', (event) => {
-    // If no selected ships, don't handle event
-    if (!selectedShip) return;
+  /* --- Placing selected ship --- */
 
-    const tile = event.target.closest('.tile');
-    if (!tile) return
+  const board = document.querySelector('.placing-board')
+
+  board.addEventListener('mouseover', (e) => {
+    const tile = e.target.closest('.tile');
+    // A tile is and ship are selected else, exit
+    if (!tile ||  !selectedShip) return;
 
     const x = parseInt(tile.dataset.x);
     const y = parseInt(tile.dataset.y);
     const shipLength = parseInt(selectedShip.dataset.shipLength) ;
     const shipDirection = selectedShip.dataset.shipDirection;
 
+
     // gameflow should be an imported instance of GameFlow
-    validity = gameflow.player.gameboard.checkCoords(
+    const validity = gameflow.player.gameboard.checkCoords(
       x, y, shipLength, shipDirection
     );
-
-    console.log(validity? "valid" : "invalid")
-
+  
+    paintPreview(x, y, shipLength, shipDirection, validity)
   })
 
+  board.addEventListener('mouseleave', clearPreview);
+
+  // Showing validity of placement tile(s)
+  function paintPreview(posX, posY, length, direction, isValid) {
+    clearPreview();
+    const isHorizontal = direction === "horizontal";
+    
+    for (let i = 0; i < length; i++) {
+      const x = isHorizontal ? posX : posX + i;
+      const y = isHorizontal ? posY + i : posY;
+  
+      const tile = document.querySelector(`[data-x="${x}"][data-y="${y}"]`);
+      if (tile) {
+        tile.classList.add(isValid ? 'valid-preview' : 'invalid-preview');
+      }
+    }
+  }
+  
+  function clearPreview() {
+    document.querySelectorAll('.valid-preview, .invalid-preview')
+      .forEach(tile => tile.classList.remove('valid-preview', 'invalid-preview'));
+  }
 }
